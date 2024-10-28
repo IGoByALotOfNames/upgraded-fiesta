c7579d57e873e779e6f573aed0e5f071d5c4bad1
IGoByALotOfNames <helloworld202110@zohomail.com>
2024-10-28 14:46:44 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ import google.generativeai as genai
 genai.configure(api_key=st.secrets["API_KEY"])
 model = genai.GenerativeModel("gemini-1.5-flash")
 response = model.generate_content("Write a story about a magic backpack.")
-print(response.text)
+st.markdown(response.text)
